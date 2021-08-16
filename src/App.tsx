a3ff70177cd998b71331f92de9778d194bf58d79
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-08-17 08:00:49 +0900

    useQueryのcustomHookを作成。

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
-function App() {
-  return <div className=""></div>
+import { VFC } from 'react'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query-devtools'
+import { BrowserRouter } from 'react-router-dom'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+})
+
+const App: VFC = () => {
+  return (
+    <div className="flex justify-center items-center flex-col min-h-screen text-gray-600 text-sm  font-mono">
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter></BrowserRouter>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </div>
+  )
 }
 
 export default App
